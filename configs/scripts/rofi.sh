057df0651f1d53f43a56430f51841df399174e07
messor <igoshin.rusl@yandex.ru>
2025-08-17 20:18:17 +0300

    new hyprland config

---
+++ configs/scripts/rofi.sh
@@ -0,0 +1,8 @@
+dir="$HOME/.config/hypr/themes/catpuccin/rofi"
+theme='config'
+
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/${theme}.rasi \
+    -icon-theme 
--- themes/hypr_arch/rofi/launchpad/launcher.sh
+++
@@ -1,8 +0,0 @@
-dir="$HOME/.config/hypr/themes/hypr_arch/rofi/launchpad"
-theme='launchpad'
-
-## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi \
-    -icon-theme Tela-circle-black
